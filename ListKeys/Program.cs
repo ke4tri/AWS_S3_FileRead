9865fc6514c05a39b5acdf7ebc6f4bf2bb0334b1
Wayne Collier <waynec@patientfocus.com>
2019-09-26 15:20:26 -0500

    setup

---
+++ ListKeys/Program.cs
@@ -0,0 +1,65 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace ListKeys
+{
+    class ListObjectsTest
+    {
+        private const string bucketName = "new-bucket-3b3e97c5";
+        private const string keyName = "Test";
+        // Specify your bucket region (an example region is shown).
+        private static readonly Amazon.RegionEndpoint bucketRegion = Amazon.RegionEndpoint.USEast1;
+
+        private static IAmazonS3 client;
+
+        public static void Main()
+        {
+            client = new AmazonS3Client(bucketRegion);
+            ListingObjectsAsync().Wait();
+
+             
+        }
+
+        static async Task ListingObjectsAsync()
+        {
+            try
+            {
+                ListObjectsV2Request request = new ListObjectsV2Request
+                {
+                    BucketName = bucketName,
+                    MaxKeys = 10
+                };
+                ListObjectsV2Response response;
+                do
+                {
+                    response = await client.ListObjectsV2Async(request);
+
+                    // Process the response.
+                    foreach (S3Object entry in response.S3Objects)
+                    {
+                        Console.WriteLine("key = {0} size = {1}",
+                            entry.Key, entry.Size);
+                    }
+                    Console.WriteLine("Next Continuation Token: {0}", response.NextContinuationToken);
+                    Console.ReadLine();
+                    request.ContinuationToken = response.NextContinuationToken;
+                   
+                } while (response.IsTruncated);
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                Console.WriteLine("S3 error occurred. Exception: " + amazonS3Exception.ToString());
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.ToString());
+                Console.ReadKey();
+            }
+        }
+
+        
+    }
+}
